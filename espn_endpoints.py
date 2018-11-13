bba131b2f1e7062b5181b2874e16cac42cfa1b48
geoff goss <geoff@duckwerks.com>
2018-11-12 21:00:36 -0800

    update endpoints to be more generic

--- espn_endpoints.py
+++ espn_endpoints.py
@@ -1,8 +1,10 @@
 endpoints = {
-'roku': 'http://watch.product.api.qa.espn.com/api/product/v3/watchespn/roku/featured?entitlements=ESPN_PLUS&appVersion=2.3&authStates=mvpd_previous&lang=en&deviceType=settop&favoriteSport=&favoriteTeam=&zipcode=94850&countryCode=US&tz=-8&iapPackages=ESPN_PLUS',
-'atvl': 'http://watch.product.api.qa.espn.com/api/product/v3/appletvl/espn/featured?tz=-0800&deviceType=settop&countryCode=us&zipCode=97204&entitlements=ESPN_PLUS&iapPackages=ESPN_PLUS,ESPN_PLUS_NHL&lang=en',
+'roku': 
+'http://watch.product.api.qa.espn.com/api/product/v3/watchespn/roku/featured?entitlements=ESPN_PLUS',
+'atvl':
+'http://watch.product.api.qa.espn.com/api/product/v3/appletvl/espn/featured?entitlements=ESPN_PLUS',
 'tizen':
-'https://watch.product.api.qa.espn.com/api/product/v3/tvos/watchespn/featured?authStates=mvpd_previous&entitlements=fallback_subscription_activation_tda%2CESPN_PLUS_NHL&deviceType=unknown&debug=true',
+'http://watch.product.api.qa.espn.com/api/product/v3/tvos/watchespn/featured?entitlements=ESPN_PLUS',
 'xbox':
-'https://watch.product.api.qa.espn.com/api/product/v3/tvos/watchespn/featured?tz=-7&lang=en&zipcode=85262&countryCode=us&deviceType=desktop&entitlements=ESPN_PLUS&iapPackages=ESPN_PLUS%2CESPN_PLUS_NHL'
+'http://watch.product.api.qa.espn.com/api/product/v3/tvos/watchespn/featured?entitlements=ESPN_PLUS',
 }
