8a6ea17fcda402ff3a615b866136824c89e02dce
geoff goss <geoff@duckwerks.com>
2018-11-12 20:21:40 -0800

    update script, add helper endpoint file, gitignore, etc

---
+++ espn_endpoints.py
@@ -0,0 +1,8 @@
+endpoints = {
+'roku': 'http://watch.product.api.qa.espn.com/api/product/v3/watchespn/roku/featured?entitlements=ESPN_PLUS&appVersion=2.3&authStates=mvpd_previous&lang=en&deviceType=settop&favoriteSport=&favoriteTeam=&zipcode=94850&countryCode=US&tz=-8&iapPackages=ESPN_PLUS',
+'atvl': 'http://watch.product.api.qa.espn.com/api/product/v3/appletvl/espn/featured?tz=-0800&deviceType=settop&countryCode=us&zipCode=97204&entitlements=ESPN_PLUS&iapPackages=ESPN_PLUS,ESPN_PLUS_NHL&lang=en',
+'tizen':
+'https://watch.product.api.qa.espn.com/api/product/v3/tvos/watchespn/featured?authStates=mvpd_previous&entitlements=fallback_subscription_activation_tda%2CESPN_PLUS_NHL&deviceType=unknown&debug=true',
+'xbox':
+'https://watch.product.api.qa.espn.com/api/product/v3/tvos/watchespn/featured?tz=-7&lang=en&zipcode=85262&countryCode=us&deviceType=desktop&entitlements=ESPN_PLUS&iapPackages=ESPN_PLUS%2CESPN_PLUS_NHL'
+}
--- rec_rows.py
+++ rec_rows.py
@@ -2,18 +2,33 @@ import requests
 import json
 import time
 import argparse
+import espn_endpoints
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--token", help="dss session token", type=str, default="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJjb250ZXh0Ijp7InByb2ZpbGVzIjpbeyJhY3RpdmUiOnRydWUsInR5cGUiOiJ1cm46YmFtdGVjaDpwcm9maWxlIiwiaWQiOiJ1c2VyXzEiLCJwYXJlbnRhbF9jb250cm9scyI6eyJlbmFibGVkIjpmYWxzZX19XX19.D-oSieYZYy58YZBMv_lSm_i0-LR1zqSCNK3r1XRl7V8")
-parser.add_argument("--url", help="url", type=str, default="http://watch.product.api.qa.espn.com/api/product/v3/watchespn/roku/featured?entitlements=ESPN_PLUS&appVersion=2.3&authStates=mvpd_previous&lang=en&deviceType=settop&favoriteSport=&favoriteTeam=&zipcode=94850&countryCode=US&tz=-8&iapPackages=ESPN_PLUS")
+parser.add_argument("--url", help="optional url - this overrides the endpoint option", type=str)
+parser.add_argument("--endpoint", help="choose a platform for the pageAPI endpoint", choices=espn_endpoints.endpoints.keys())
 args = parser.parse_args()
 
+if args.url:
+    url = args.url
+elif args.endpoint:
+    url = espn_endpoints.endpoints[args.endpoint]
+else:
+    print 'either add provide a url or choose an endpoint'
+    exit()
+
+print '==========='
+print 'SAMPLE CURL:'    
+print 'curl -H \"dss-session-token:%s\" \"%s\"' % (args.token, url)
 print '==========='
 
+exit()
 s = requests.Session()
 s.headers.update({'dss-session-token': args.token})
-resp_json = s.get(args.url).json()
+resp_json = s.get(url).json()
 
+print 'Recommended for You content:'
 rec_buckets = int(0)
 for bucket in resp_json['page']['buckets']:
     if bucket['name'] == 'Recommended for You':
@@ -25,10 +40,12 @@ for bucket in resp_json['page']['buckets']:
 if rec_buckets == 0:
     print 'No Recommended for You row found'
 
-filename = 'output/output_dss_token_%s.json' % time.strftime('%Y%m%d_%H%M_%S')
+filename = 'output/output_rec_rows_%s.json' % time.strftime('%Y%m%d_%H%M_%S')
 text_file = open(filename, "w")
 text_file.write(json.dumps(resp_json, indent=2))
 text_file.close()
 
 print '==========='
 print 'output saved to %s' % filename
+print '==========='
+exit()
